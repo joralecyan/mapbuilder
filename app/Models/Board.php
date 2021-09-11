840316fe3bc5d4bac175baaab7253b70c5fe7735
Jor Aleqyan <josh.alecyan@gmail.com>
2021-09-11 12:10:07 +0400

    Added board and game models

---
+++ app/Models/Board.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Board extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'boards';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['user_id', 'game_id', 'data'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function game(): BelongsTo
+    {
+        return $this->belongsTo(Game::class, 'game_id', 'id');
+    }
+}
